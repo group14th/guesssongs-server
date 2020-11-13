eee06ec4e6fbea35c9dc9726765d0fc374de9f62
normndakbr <normnd.akbr2@gmail.com>
2020-11-13 12:59:19 +0700

    added websocket - socket.io for login and list of Logged In Users

--- server/app.js
+++ server/app.js
@@ -4,12 +4,31 @@ const app = express()
 const PORT = 3000
 const http = require("http").createServer(app)
 const routes = require("./routes")
+const io = require("socket.io")(http)
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
 app.use("/", routes)
+
+let onlineUsers = [];
+let rooms = [];
+
+io.on("connection", (socket) => {
+    //console.log('a user connected');
+    socket.on('letsGo', (data) => { 
+      const newUser = {
+          id: socket.id,
+          name: data
+      }
+      onlineUsers.push(newUser);
+      socket.emit('jumpto-lobby')
+      io.emit('update-onlineUsers', onlineUsers)
+    })
+    socket.on('get-onlineUsers', () => {
+      socket.emit('update-onlineUsers', onlineUsers)
+    })
+});
 
 http.listen(PORT, () => {
   console.log(`LOVE U ${PORT}`)
